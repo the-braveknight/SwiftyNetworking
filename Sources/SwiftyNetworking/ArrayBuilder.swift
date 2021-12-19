12cbe32cbf1b5c99d25466dad00a544449e9f479
Zaid <zaid.riadh@gmail.com>
2021-12-20 00:11:38 +0300

    Add more functions to ArrayBuilder

--- Sources/SwiftyNetworking/ArrayBuilder.swift
+++ Sources/SwiftyNetworking/ArrayBuilder.swift
@@ -1,6 +1,6 @@
 //
 //  ArrayBuilder.swift
-//  
+//
 //
 //  Created by Zaid Rahhawi on 11/7/21.
 //
@@ -9,7 +9,27 @@ import Foundation
 
 @resultBuilder
 public struct ArrayBuilder {
-    public static func buildBlock<Item>(_ items: Item...) -> [Item] {
-        return items
+    static func buildExpression<Item>(_ element: Item) -> [Item] {
+        return [element]
+    }
+    
+    static func buildOptional<Item>(_ component: [Item]?) -> [Item] {
+        return component ?? []
+    }
+    
+    static func buildEither<Item>(first component: [Item]) -> [Item] {
+        return component
+    }
+    
+    static func buildEither<Item>(second component: [Item]) -> [Item] {
+        return component
+    }
+    
+    static func buildArray<Item>(_ components: [[Item]]) -> [Item] {
+        return Array(components.joined())
+    }
+    
+    static func buildBlock<Item>(_ components: [Item]...) -> [Item] {
+        return Array(components.joined())
     }
 }
